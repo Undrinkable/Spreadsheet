03668bb0cf488f2b070eb1f1a0137d03a748def5
Hannah <ha.al.bu@gmail.com>
2016-11-19 13:24:53 -0600

    UI updates, add row and add column

--- app/src/main/java/com/me/hannah/spreadsheet/SpreadsheetActivity.java
+++ app/src/main/java/com/me/hannah/spreadsheet/SpreadsheetActivity.java
@@ -1,13 +1,10 @@
 package com.me.hannah.spreadsheet;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,31 +14,30 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class SpreadsheetActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         SpreadsheetFragment.OnFragmentInteractionListener {
 
-    private String[][] _model = new String[][]{
-            new String[]{"A1", "A2", "A3", "A4"},
-            new String[]{"B1", "B2", "B3", "B4"},
-            new String[]{"C1", "C2", "C3", "C4"},
-            new String[]{"D1", "D2", "D3", "D4"},
-    };
-    private TableLayout _tableLayout;
-    private EditText _editText;
+    private String[][] _model = new String[][]{new String[]{"A1", "A2", "A3", "A4"},
+            new String[]{"B1", "B2", "B3", "B4"}, new String[]{"C1", "C2", "C3", "C4"},
+            new String[]{"D1", "D2", "D3", "D4"},};
+    private ViewGroup _tableLayout;
+    private EditText _editCell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View contentView = getView();
-        setContentView(contentView);
+        setContentView(inflateView());
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,14 +46,14 @@ public class SpreadsheetActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                reload();
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
+                        R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -66,32 +62,52 @@ public class SpreadsheetActivity extends AppCompatActivity
     }
 
     @NonNull
-    private View getView() {
+    private View inflateView() {
         View contentView = LayoutInflater.from(this).inflate(R.layout.activity_spreadsheet, null);
-        _editText = (EditText) contentView.findViewById(R.id.editText);
+
+        contentView.findViewById(R.id.add_row).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addRow();
+            }
+        });
+        contentView.findViewById(R.id.add_column).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addColumn();
+            }
+        });
+
+        _editCell = (EditText) contentView.findViewById(R.id.editText);
         _tableLayout = (TableLayout) contentView.findViewById(R.id.tableLayout);
-        for (int i = 0; i < 4; i++) {
-            TableRow row = new TableRow(this);
-            row.setShowDividers(LinearLayout.SHOW_DIVIDER_BEGINNING | LinearLayout.SHOW_DIVIDER_END | LinearLayout.SHOW_DIVIDER_MIDDLE);
-            row.setDividerDrawable(new ColorDrawable(Color.GRAY));
-            for (int j = 0; j < 4; j++) {
-                TextView t = new TextView(this);
-                t.setClickable(true);
-                final int x = i, y = j;
-                t.setOnClickListener(new View.OnClickListener() {
+        setupTableView();
+
+        return contentView;
+    }
+
+    private void setupTableView() {
+        _tableLayout.removeAllViews();
+        LayoutInflater inflater = LayoutInflater.from(this);
+        for (int rowIndex = 0; rowIndex < _model.length; rowIndex++) {
+
+            ViewGroup row =
+                    (ViewGroup) inflater.inflate(R.layout.spreadsheet_row, _tableLayout, false);
+            _tableLayout.addView(row);
+
+            for (int columnIndex = 0; columnIndex < _model[0].length; columnIndex++) {
+                View cell = inflater.inflate(R.layout.spreadsheet_cell, row, false);
+                row.addView(cell);
+
+                final int x = rowIndex, y = columnIndex;
+                cell.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         editCell(x, y);
                     }
                 });
-                t.setMinWidth(150);
-                row.addView(t);
+
             }
-            _tableLayout.addView(row);
         }
-        _tableLayout.setShowDividers(LinearLayout.SHOW_DIVIDER_BEGINNING | LinearLayout.SHOW_DIVIDER_END | LinearLayout.SHOW_DIVIDER_MIDDLE);
-        _tableLayout.setDividerDrawable(new ColorDrawable(Color.GRAY));
-        return contentView;
     }
 
     @Override
@@ -101,22 +117,22 @@ public class SpreadsheetActivity extends AppCompatActivity
     }
 
     private void editCell(final int x, final int y) {
-        _editText.setVisibility(View.VISIBLE);
-        _editText.setText(_model[x][y]);
-        _editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        _editCell.setVisibility(View.VISIBLE);
+        _editCell.setText(_model[x][y]);
+        _editCell.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if ((keyEvent == null || keyEvent.getAction() == KeyEvent.ACTION_DOWN)
-                        && i == KeyEvent.KEYCODE_ENDCALL) {
-                    _model[x][y] = _editText.getText().toString();
+                if ((keyEvent == null || keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        i == KeyEvent.KEYCODE_ENDCALL) {
+                    _model[x][y] = _editCell.getText().toString();
                     fillSpreadsheet();
-                    _editText.setVisibility(View.INVISIBLE);
+                    _editCell.setVisibility(View.INVISIBLE);
                     return true;
                 }
                 return false;
             }
         });
-        _editText.callOnClick();
+        _editCell.callOnClick();
     }
 
     private void fillSpreadsheet() {
@@ -144,7 +160,6 @@ public class SpreadsheetActivity extends AppCompatActivity
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
@@ -164,14 +179,36 @@ public class SpreadsheetActivity extends AppCompatActivity
     }
 
     private void clear() {
-
+        Toast.makeText(this, "Clear pressed", Toast.LENGTH_SHORT).show();
     }
 
     private void reload() {
-
+        Toast.makeText(this, "Reload pressed", Toast.LENGTH_SHORT).show();
     }
 
     private void save() {
+        Toast.makeText(this, "Save pressed", Toast.LENGTH_SHORT).show();
+    }
 
+    private void addRow() {
+        String[][] newModel = new String[_model.length + 1][_model[0].length];
+        for (int rowIndex = 0; rowIndex < _model.length; rowIndex++) {
+            newModel[rowIndex] = Arrays.copyOf(_model[rowIndex], _model[rowIndex].length);
+        }
+        newModel[_model.length] = new String[_model[0].length];
+
+        _model = newModel;
+        setupTableView();
+        fillSpreadsheet();
+    }
+
+    private void addColumn() {
+        String[][] newModel = new String[_model.length][_model[0].length + 1];
+        for (int rowIndex = 0; rowIndex < _model.length; rowIndex++) {
+            newModel[rowIndex] = Arrays.copyOf(_model[rowIndex], _model[rowIndex].length + 1);
+        }
+        _model = newModel;
+        setupTableView();
+        fillSpreadsheet();
     }
 }
